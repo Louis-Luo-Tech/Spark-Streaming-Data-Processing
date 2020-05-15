cca5975bf9c9624d2b5987bcb63978c99997a722
Louis-Luo-Tech <xiangluoapply@gmail.com>
2020-05-15 03:28:52 -0700

    checkpoint

--- src/main/scala/com/louis/spark/NerworkWordCount.scala
+++ src/main/scala/com/louis/spark/NerworkWordCount.scala
@@ -7,9 +7,9 @@ object NerworkWordCount {
   def main(args: Array[String]): Unit = {
     //    StreamingExamples.setStreamingLogLevels()
 
-    val sparkconf: SparkConf = new SparkConf().setMaster("local[2]").setAppName("NerworkWordCount") //local[?] ?>=2 is needed because there is a receiver that need thread
-    val ssc = new StreamingContext(sparkconf, Seconds(1))
-    val lines = ssc.socketTextStream("localhost", 9977)
+    val sparkconf: SparkConf = new SparkConf().setMaster("local[10]").setAppName("NerworkWordCount") //local[?] ?>=2 is needed because there is a receiver that need thread
+    val ssc = new StreamingContext(sparkconf, Seconds(5))
+    val lines = ssc.socketTextStream("localhost", 8222)
     val words = lines.flatMap(_.split(" "))
     val wordscount = words.map(x => (x, 1)).reduceByKey(_ + _)
     wordscount.print()
