da75a9efa7cc49eccc6e7f3abea0afb0ea213193
Louis-Luo-Tech <xiangluoapply@gmail.com>
2020-05-19 23:30:48 -0700

    commit

--- src/main/scala/com/louis/spark/FlumePullWordCount.scala
+++ src/main/scala/com/louis/spark/FlumePullWordCount.scala
@@ -1,12 +1,11 @@
 package com.louis.spark
 
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.flume.FlumeUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.flume.FlumeUtils
 
-object FlumePushWordCount {
+object FlumePullWordCount {
   def main(args: Array[String]): Unit = {
-
     if(args.length != 2){
       System.err.println("Usage: FlumePushWordCount <hostname> <port>")
       System.exit(1)
@@ -15,14 +14,14 @@ object FlumePushWordCount {
     val Array(hostname,port) = args
 
 
-    val sparkconf = new SparkConf()//.setMaster("local[10]").setAppName("FlumePushWordCount")
+    val sparkconf = new SparkConf().setMaster("local[10]").setAppName("FlumePushWordCount")
     val ssc = new StreamingContext(sparkconf,Seconds(5))
 
-    val flumeStream = FlumeUtils.createStream(ssc,hostname,port.toInt)
+    val flumeStream = FlumeUtils.createPollingStream(ssc,hostname,port.toInt)
 
     flumeStream.map(x=> new String(x.event.getBody.array()).trim)
-        .flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
-        .print()
+      .flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+      .print()
 
     ssc.start()
     ssc.awaitTermination()
