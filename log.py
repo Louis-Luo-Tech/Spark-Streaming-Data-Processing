a63f616e68f01aabcffb0e7cd78430d1c4bfa238
Louis-Luo-Tech <xiangluoapply@gmail.com>
2020-05-29 16:08:23 -0700

    kafka

---
+++ log.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 06:12:43 2020
+
+@author: xiangluo
+"""
+
+import time
+import os
+
+while True:
+    time.sleep(6)
+    os.system('sh /Users/xiangluo/generatelog.sh')
